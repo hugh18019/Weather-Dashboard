afb4eefb32442a2ba687c1fd4b67561644d6eb41
Huiran Lin <hugh80082@gmail.com>
2021-07-06 04:18:10 -0700

    clicking on each past search displays its weather data

--- assets/js/result.js
+++ assets/js/result.js
@@ -4,27 +4,45 @@ var humidityEl = $( '#humidity' );
 var windSpeedEl = $( '#wind-speed' );
 var uviEl = $( '#uvi' );
 
-var pastSearchesUlEl = $( '.list-group' );
+var pastSearchesListEl = $( '.list-group' );
 
-var pastSearches = [];
-localStorage.setItem( "pastSearches", JSON.stringify( pastSearches ) );
+init();
 
-writeWeather();
+function init() {
+    var pastSearches = JSON.parse( localStorage.getItem( "pastSearches" ) );
+    if( !pastSearches ) {
+        var pastSearches = [];
+        localStorage.setItem( "pastSearches", JSON.stringify( pastSearches ) );
+    }
+    else {
+        for( var each of pastSearches ) {
+            displaySearchHistory( each );
+        }
+    }
+}
+
+var key = localStorage.getItem( "currentSearchCity" );
+writeWeather( key );
 
 // Outputs the weather data of the current search
 // Add add the current search to the list of search history
-function writeWeather() {
-    var key = localStorage.getItem( "currentSearchCity" );
+function writeWeather( key ) {
+    
     var weatherDataObj = JSON.parse( localStorage.getItem( key + "_weatherData" ) );
     cityDateEl.text( weatherDataObj.city );
 
-    tempEl.append( " " + weatherDataObj.temperature );
+    tempEl.text( "Temperature: " + weatherDataObj.temperature );
 
-    humidityEl.append( " " + weatherDataObj.humidity );
-    windSpeedEl.append( " " + weatherDataObj.wind_speed );
-    uviEl.append( " " + weatherDataObj.uv_index );
+    humidityEl.text( "Humidity: " + weatherDataObj.humidity );
+    windSpeedEl.text( "Wind Speed: " + weatherDataObj.wind_speed );
+    uviEl.text( "UV Index: " + weatherDataObj.uv_index );
 
-    addToSearchHistory( key );
+    var pastSearches = JSON.parse( localStorage.getItem( "pastSearches" ) );
+    if( pastSearches.length === 0 || !pastSearches.includes( key ) ) {
+
+        addToSearchHistory( key );
+    }
+    
 }
 
 // Add the current search to the array of past searches and stores the array
@@ -32,13 +50,29 @@ function writeWeather() {
 function addToSearchHistory( currentCity ) {
     var pastSearches = JSON.parse( localStorage.getItem( "pastSearches" ) );
     pastSearches.push( currentCity );
-    
+    localStorage.setItem( "pastSearches", JSON.stringify( pastSearches ) );
+
+    displaySearchHistory( currentCity );
+}
+
+
+function displaySearchHistory( currentCity ) {
     var searchEl = $( '<button>' );
     searchEl.text( currentCity );
     searchEl.attr( 'type', 'button' );
     searchEl.addClass( "list-group-item" );
     searchEl.addClass( "list-group-item-action" );
+    searchEl.addClass( "show-data-btn" );
 
-    pastSearchesUlEl.append( searchEl );
-
+    pastSearchesListEl.append( searchEl );
 }
+
+
+function displayWeatherData( event ) {
+    var btnClicked = $( event.target ) ;
+    var cityClicked = btnClicked.text();
+    writeWeather( cityClicked );
+}
+
+
+pastSearchesListEl.on( 'click', '.show-data-btn', displayWeatherData );
