be032b292a488625ec3b33e4b247524361681af1
Huiran Lin <hugh80082@gmail.com>
2021-07-06 03:23:42 -0700

    add current search to array of past searches

--- assets/js/result.js
+++ assets/js/result.js
@@ -4,10 +4,15 @@ var humidityEl = $( '#humidity' );
 var windSpeedEl = $( '#wind-speed' );
 var uviEl = $( '#uvi' );
 
+var pastSearchesUlEl = $( '.list-group' );
 
+var pastSearches = [];
+localStorage.setItem( "pastSearches", JSON.stringify( pastSearches ) );
 
 writeWeather();
 
+// Outputs the weather data of the current search
+// Add add the current search to the list of search history
 function writeWeather() {
     var key = localStorage.getItem( "currentSearchCity" );
     var weatherDataObj = JSON.parse( localStorage.getItem( key + "_weatherData" ) );
@@ -18,4 +23,22 @@ function writeWeather() {
     humidityEl.append( " " + weatherDataObj.humidity );
     windSpeedEl.append( " " + weatherDataObj.wind_speed );
     uviEl.append( " " + weatherDataObj.uv_index );
+
+    addToSearchHistory( key );
+}
+
+// Add the current search to the array of past searches and stores the array
+// in local storage
+function addToSearchHistory( currentCity ) {
+    var pastSearches = JSON.parse( localStorage.getItem( "pastSearches" ) );
+    pastSearches.push( currentCity );
+    
+    var searchEl = $( '<button>' );
+    searchEl.text( currentCity );
+    searchEl.attr( 'type', 'button' );
+    searchEl.addClass( "list-group-item" );
+    searchEl.addClass( "list-group-item-action" );
+
+    pastSearchesUlEl.append( searchEl );
+
 }
